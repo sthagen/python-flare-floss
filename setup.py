5264b6e0325d6ecd78f707358f3c22a1e0d984bb
William Ballenthin <william.ballenthin@fireeye.com>
2016-03-04 17:45:53 -0500

    setup.py: bump to 1.0.1 to figure out github releases

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = [
 
 setup(
     name='floss',
-    version='1.0.0',
+    version='1.0.1',
     description="",
     long_description="",
     author="Willi Ballenthin, Moritz Raabe",
