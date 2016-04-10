be8d3f7f652ab5be421475ee08ade2b78bfe523e
Willi Ballenthin <willi.ballenthin@gmail.com>
2016-04-10 18:50:20 +0000

    tests/base64: remove wrapper fn

--- tests/src/decode-base64/test-decode-base64.c
+++ tests/src/decode-base64/test-decode-base64.c
@@ -6,19 +6,11 @@
 #include "base64.h"
 
 
-int decode(void *out, size_t out_len, const void *in, size_t in_len) {
-    return Base64decode(out, in) == in_len;
-}
-
 char in[] = "aGVsbG8gd29ybGQ=";
 
 int main(int argc, char **argv) {
     char out[sizeof(in) + 1];
-
-    if (decode(out, sizeof(in) + 1, in, sizeof(in))) {
-        perror("failed to decode.\n");
-        return -1;
-    }
+    Base64decode(out, in);
     printf("%s\n", out);
     return 0;
 }
