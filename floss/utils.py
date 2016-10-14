7fc466bfb01158036aec2b591676bb2ec299cffd
Moritz Raabe <moritz.raabe@fireeye.com>
2016-10-14 13:51:00 +0200

    patch readMemory to get rid of AAA strings

--- floss/utils.py
+++ floss/utils.py
@@ -1,5 +1,47 @@
+import types
+
+import envi.memory as e_mem
+import envi.bits as e_bits
+import visgraph.pathcore as vg_path
+
+from vivisect.impemu.emulator import WorkspaceEmulator
+from vivisect.const import LOC_IMPORT
+
+
 ONE_MB = 1024 * 1024
 STACK_MEM_NAME = "[stack]"
+
+
+def flossReadMemory(self, va, size):
+    """
+    This is directly copied from vivisect/impemu/emulator.py.
+    The only change is that we return non-ASCII characters (0x90) instead of 'A's if probing the memory fails.
+    This gets rid of many AAA... false positive decoded strings.
+    :param self: emulator instance
+    :param va: virtual address of requested memory
+    :param size: size of requested memory
+    :return: requested memory or '\x90' if memory hasn't been resolved yet
+    """
+    if self.logread:
+        rlog = vg_path.getNodeProp(self.curpath, 'readlog')
+        rlog.append((self.getProgramCounter(),va,size))
+
+    # If they read an import entry, start a taint...
+    loc = self.vw.getLocation(va)
+    if loc != None:
+        lva, lsize, ltype, ltinfo = loc
+        if ltype == LOC_IMPORT and lsize == size:  # They just read an import.
+            ret = self.setVivTaint('import', loc)
+            return e_bits.buildbytes(ret, lsize)
+
+    self._useVirtAddr(va)
+
+    # Read from the emulator's pages if we havent resolved it yet
+    probeok = self.probeMemory(va, size, e_mem.MM_READ)
+    if self._safe_mem and not probeok:
+        return '\0x90' * size  # 0x90 is non-ASCII and NOP instruction in x86
+
+    return e_mem.MemoryObject.readMemory(self, va, size)
 
 
 def makeEmulator(vw):
@@ -7,6 +49,7 @@ def makeEmulator(vw):
     create an emulator using consistent settings.
     """
     emu = vw.getEmulator(logwrite=True)
+    emu.readMemory = types.MethodType(flossReadMemory, emu)  # patch readMemory function in emulator object
     removeStackMemory(emu)
     emu.initStackMemory(stacksize=int(0.5 * ONE_MB))
     emu.setStackCounter(emu.getStackCounter() - int(0.25 * ONE_MB))
